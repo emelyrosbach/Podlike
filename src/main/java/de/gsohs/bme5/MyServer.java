5ea4f65686c7f74a4ccd22c034f128c93a2db004
emelyrosbach <emelyrosbach@web.de>
2020-11-12 16:31:17 +0100

    server zeug

--- src/main/java/de/gsohs/bme5/MyServer.java
+++ src/main/java/de/gsohs/bme5/MyServer.java
@@ -4,9 +4,10 @@ import io.javalin.Javalin;
 public class MyServer {
     public static void main(String[] args) {
         Javalin javalin = Javalin.create().start(8080);
-        javalin.get("/hello", ctx -> ctx.result("Hello World!"));
-        javalin.get("/greet/:name", ctx -> { ctx.result("Hello: " + ctx.pathParam("name")); });
-        javalin.get("/myrequest", ctx -> ctx.result(ctx.header("key")));
-        javalin.post("/formpost", ctx -> ctx.result(ctx.body()));
+        javalin.get("/", ctx -> ctx.render("landingpage.html"));
+        javalin.get("/podcast", ctx -> ctx.render("podcast.html"));
+        javalin.get("/signin", ctx -> ctx.render("signin.html"));
+        javalin.get("/signup", ctx -> ctx.render("signup.html"));
+
     }
 }
