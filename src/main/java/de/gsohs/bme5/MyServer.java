d90fac5b4d5d212bd36fdff6f5cd40cfb7a2676f
emelyrosbach <emelyrosbach@web.de>
2021-01-14 15:49:59 +0100

    final stuff

--- src/main/java/de/gsohs/bme5/MyServer.java
+++ src/main/java/de/gsohs/bme5/MyServer.java
@@ -7,28 +7,32 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Query;
 import java.util.List;
+import java.util.Map;
 
 public class MyServer {
 
     public static void main(String[] args) {
         MyEmFactory emf = MyEmFactory.getInstance();
         EntityManager em = emf.getEntityManager();
-        em.getTransaction().begin();
+        /*em.getTransaction().begin();
         Podcast pd = new Podcast();
-        //pd.setPodcast_id((long) 1);
-        pd.setTitle("uno");
+        pd.setTitle("troi");
         pd.setDescription("podcast");
         em.merge(pd);
         em.getTransaction().commit();
-        em.close();
+        em.close();*/
         Javalin javalin = Javalin.create().start(8080);
         javalin.get("/", ctx -> {
-            /*Query q = em.createQuery("select p from Podcast p order by p.podcast_id asc");
-            return (List<Podcast>) q.getResultList();*/
-            //return em.createQuery("SELECT a FROM Podcast a", Podcast.class).getResultList();
-            //ctx.render("landingpage.html",Map.of("podcasts",new String[] {"apples","bananas","peaches"}));
+            Query q = em.createQuery("select p from Podcast p order by p.podcast_id asc");
+            List <Podcast> result = (List<Podcast>) q.getResultList();
+            ctx.render("landingpage.html", Map.of("podcasts", result));
         });
-        javalin.get("/podcast", ctx -> ctx.render("podcast.html"));
+        javalin.get("/podcast/:id", ctx -> {
+            Long podcastId = Long.parseLong(ctx.pathParam("id"));
+            Query q = em.createQuery("select p from Podcast p where p.podcast_id = :podcastId");
+            Podcast result = (Podcast) q.setParameter("podcastId", podcastId).getSingleResult();
+            ctx.render("podcast.html", Map.of("podcast", result));
+        });
         javalin.get("/signin", ctx -> ctx.render("signin.html"));
         javalin.get("/signup", ctx -> ctx.render("signup.html"));
     }
